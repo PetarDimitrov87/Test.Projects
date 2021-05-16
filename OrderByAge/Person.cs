3be1d334b3131c134afba7865e5678f86b496275
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 21:23:45 +0300

    commit Order by Age project

---
+++ OrderByAge/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderByAge
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} with ID: {Id} is {Age} years old.";
+        }
+    }
+}
---
+++ OrderByAge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OrderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                string name = input[0];
+                string id = input[1];
+                int age = int.Parse(input[2]);
+
+                Person person = new Person
+                {
+                    Name = name,
+                    Id = id,
+                    Age = age
+                };
+
+                persons.Add(person);
+            }
+
+            List<Person> sortedPersons = persons
+                .OrderBy(a => a.Age)
+                .ToList();
+
+            foreach (var person in sortedPersons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
