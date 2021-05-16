ee9f1747742babf6f71241604b3170142312d8fc
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 21:17:37 +0300

    commit Students project

---
+++ Students/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Student> students = new List<Student>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] studentData = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Student student = new Student
+                {
+                    FirstName = studentData[0],
+                    LastName = studentData[1],
+                    Grade = double.Parse(studentData[2])
+                };
+
+                students.Add(student);
+            }
+
+            List<Student> sortedStudents = students
+                .OrderByDescending(x => x.Grade)
+                .ToList();
+
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ Students/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Students
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Grade { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: {Grade:F2}";
+        }
+    }
+}
