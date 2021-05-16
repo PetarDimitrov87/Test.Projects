711d5cd9a0358739fd7fadc6e0578dd71b52c06c
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 18:41:35 +0300

    commit Valid Usernames project

---
+++ ValidUsernames/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var username in usernames)
+            {
+                if (IsValid(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+
+        private static bool IsValid(string username)
+        {
+            return HasValidLength(username) && ContainsValidSymbols(username);
+        }
+
+        private static bool ContainsValidSymbols(string username)
+        {
+            foreach (var symbol in username)
+            {
+                if (!char.IsLetterOrDigit(symbol) &&
+                    symbol != '-' &&
+                    symbol != '_')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static bool HasValidLength(string username)
+        {
+            return username.Length >= 3 && username.Length <= 16;
+        }
+    }
+}
