e6a1608b7430b613188fcbcf58bcb1d5e70e2eaa
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 18:22:20 +0300

    commit Array Rotation project

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int rotations = int.Parse(Console.ReadLine());
+
+            rotations = rotations % arr.Length;
+
+            for (int rotation = 0; rotation < rotations; rotation++)
+            {
+                string firstElement = arr[0];
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    arr[i - 1] = arr[i];
+                }
+
+                arr[arr.Length - 1] = firstElement;
+            }
+
+            foreach (string element in arr)
+            {
+                Console.Write($"{element} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
