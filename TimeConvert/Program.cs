53e48e4e11edd61254d5c0486773259617197ae9
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 18:45:53 +0300

    commit Time Convert project

---
+++ TimeConvert/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TimeConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes = int.Parse(Console.ReadLine());
+
+            string time = TimeConvert(minutes);
+
+            Console.WriteLine(time);
+        }
+
+        private static string TimeConvert(int minutes)
+        {
+            if (minutes < 0)
+            {
+                return "Error. Time cannot be negative.";
+            }
+            
+            int hours = minutes / 60;
+            minutes = minutes % 60;
+
+            return $"{hours}:{minutes}";
+        }
+    }
+}
