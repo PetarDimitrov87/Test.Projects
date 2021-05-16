67807fd9c6853711b1c54bb1c54352a4e8d503c4
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 20:45:02 +0300

    commit Smallest of Three Numbers project

---
+++ SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int minNum = GetSmallestNumber(firstNum, secondNum, thirdNum);
+
+            Console.WriteLine(minNum);
+        }
+
+        private static int GetSmallestNumber(int first, int second, int third)
+        {
+            int min = Math.Min
+                (Math.Min(first, second), third);
+
+            return min;
+        }
+    }
+}
