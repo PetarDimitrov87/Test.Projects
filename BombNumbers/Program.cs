b19003902a0b78399a3110731c504edf413f2aac
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 18:33:50 +0300

    commit Bomb Numbers project

---
+++ BombNumbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] bombData = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int bombNumber = bombData[0];
+            int bombPower = bombData[1];
+
+            while (true)
+            {
+                int index = numbers.IndexOf(bombNumber);
+
+                if (index == -1)
+                {
+                    break;
+                }
+
+                int startIndex = index - bombPower;
+
+                if (startIndex < 0)
+                {
+                    startIndex = 0;
+                }
+
+                int count = 2 * bombPower + 1;
+
+                if (count > (numbers.Count - startIndex))
+                {
+                    count = numbers.Count - startIndex;
+                }
+
+                numbers.RemoveRange(startIndex, count);
+            }
+
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
