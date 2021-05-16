2b142bbebb36830a430068934c6a8e4f9bf379b0
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 18:13:52 +0300

    commit ZigZag Arrays project

---
+++ ZigZagArrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ZigZagArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] firstArr = new int[n];
+            int[] secondArr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int firstNumber = numbers[0];
+                int secondNumber = numbers[1];
+
+                if (i % 2 == 0)
+                {
+                    firstArr[i] = firstNumber;
+                    secondArr[i] =secondNumber;
+                }
+                else
+                {
+                    firstArr[i] = secondNumber;
+                    secondArr[i] = firstNumber;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", firstArr));
+            Console.WriteLine(string.Join(" ", secondArr));
+        }
+    }
+}
