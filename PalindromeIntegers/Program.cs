83e72967e029c3653744a6b0f4b41f03a3166dd1
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 21:00:00 +0300

    commit Palindrome Integers project

---
+++ PalindromeIntegers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    break;
+                }
+
+                Console.WriteLine(IsPalindrome(line));
+            }
+        }
+
+        private static bool IsPalindrome(string input)
+        {
+            for (int i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[input.Length - 1 - i])
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+    }
+}
