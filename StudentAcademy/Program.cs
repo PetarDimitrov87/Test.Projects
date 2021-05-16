df15f46793ea3fa68401f100f2aa9c6ee7d57051
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 18:47:35 +0300

    commit Student Academy project

---
+++ StudentAcademy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (!students.ContainsKey(studentName))
+                {
+                    students.Add(studentName, new List<double>());
+                }
+
+                students[studentName].Add(grade);
+            }
+
+            Dictionary<string, double> sortedStudents = students
+                .Select(s => new KeyValuePair<string, double>(s.Key, s.Value.Average()))
+                .Where(s => s.Value >= 4.5)
+                .OrderByDescending(s => s.Value)
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine($"{student.Key} -> {student.Value:F2}");
+            }
+        }
+    }
+}
