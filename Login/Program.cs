26b79fc90d1f4d807e229f6595365208133cd4c9
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 21:27:27 +0300

    commit Login project

---
+++ Login/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            string password = "";
+
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                password += username[i];
+            }           
+
+            int errorCount = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (errorCount >= 3)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+
+                if (input == password)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    errorCount++;
+                }             
+            }  
+        }
+    }
+}
