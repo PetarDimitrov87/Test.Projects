4c393fd4cf6835e11ee9ae0dae7eb1021b955cf5
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-16 21:15:22 +0300

    commit Articles 2.0 project

---
+++ Articles2.0/Article.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Articles2._0
+{
+    public class Article
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
---
+++ Articles2.0/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Articles2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Article> articles = new List<Article>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] articleData = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                Article article = new Article
+                {
+                    Title = articleData[0],
+                    Content = articleData[1],
+                    Author = articleData[2]
+                };
+
+                articles.Add(article);
+            }
+
+            string sortingCriteria = Console.ReadLine();
+
+            List<Article> sorted = new List<Article>();
+
+            if (sortingCriteria == "title")
+            {
+                sorted = articles
+                    .OrderBy(x => x.Title)
+                    .ToList();
+            }
+            else if (sortingCriteria == "content")
+            {
+                sorted = articles
+                    .OrderBy(x => x.Content)
+                    .ToList();
+            }
+            else if (sortingCriteria == "author")
+            {
+                sorted = articles
+                    .OrderBy(x => x.Author).ToList();
+            }
+
+            foreach (var article in sorted)
+            {
+                Console.WriteLine(article);
+            }
+        }
+    }
+}
+
