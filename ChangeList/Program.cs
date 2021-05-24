d778fa75ea8f320a3ba138db0bd5ded512412ef4
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-24 14:42:37 +0300

    commit Change List project

---
+++ ChangeList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] parts = input.Split();
+                string command = parts[0];
+
+                if (command == "Delete")
+                {
+                    int element = int.Parse(parts[1]);
+
+                    numbers.RemoveAll(n => n == element);
+                }
+                else if (command == "Insert")
+                {
+                    int element = int.Parse(parts[1]);
+                    int index = int.Parse(parts[2]);
+
+                    numbers.Insert(index, element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
