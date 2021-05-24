dbf5373400e835725279185c241857db979444ff
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-24 14:37:45 +0300

    commit Middle Characters project

---
+++ MiddleCharacters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string middleElement = GetMiddleChar(text);
+
+            Console.WriteLine(middleElement);
+        }
+
+        private static string GetMiddleChar(string text)
+        {
+            if (text.Length % 2 == 00)
+            {
+                return $"{text[text.Length / 2 - 1]}{text[text.Length / 2]}";
+            }
+
+            return $"{text[(text.Length - 1) / 2]}";
+        }
+    }
+}
