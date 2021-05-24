31c7441def128caa605a89ec92797bb763b2f524
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-24 14:35:18 +0300

    commit Characters in Range project

---
+++ CharactersInRange/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+
+            char start = firstSymbol;
+            char end = secondSymbol;
+
+            if (secondSymbol < firstSymbol)
+            {
+                start = secondSymbol;
+                end = firstSymbol;
+            }
+
+            PrintCharsInRange(start, end);
+        }
+
+        private static void PrintCharsInRange(char start, char end)
+        {
+            for (char i = (char)(start + 1); i < end; i++)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
