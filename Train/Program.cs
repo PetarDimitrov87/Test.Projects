0bc7af0bbb0d288520103e839a088b67c3189875
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-05-24 14:40:38 +0300

    commit Train project

---
+++ Train/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonsCount = int.Parse(Console.ReadLine());
+
+            int totalPeople = 0;
+
+            int[] peopleInTrain = new int[wagonsCount];
+
+            for (int i = 0; i < peopleInTrain.Length; i++)
+            {
+                peopleInTrain[i] = int.Parse(Console.ReadLine());
+                totalPeople += peopleInTrain[i];              
+            }
+
+            foreach (int wagon in peopleInTrain)
+            {
+                Console.Write($"{wagon} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine($"{totalPeople}");
+        }
+    }
+}
