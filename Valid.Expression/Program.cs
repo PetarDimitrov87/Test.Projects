85650225edabe1b471a0ffbfd01b65ea9978d766
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-06-02 09:19:56 +0300

    commit Valid Expression project

---
+++ Valid.Expression/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Valid.Expression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            bool result = IsValidExpression(expression);
+
+            Console.WriteLine(result);
+        }
+
+        private static bool IsValidExpression(string expression)
+        {           
+            if (expression.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            List<char> test = new List<char>();
+
+            foreach (var symbol in expression)
+            {
+                if (Char.IsUpper(symbol))
+                {
+                    test.Add(symbol);
+                }
+                else
+                {
+                    if (test.Count == 0 ||
+                        symbol != Char.ToLower(test[^1]))
+                    {
+                        return false;
+                    }
+
+                    test.RemoveAt(test.Count - 1);                  
+                }
+            }
+
+            return test.Count == 0;
+        }
+    }
+}
