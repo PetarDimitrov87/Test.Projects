7e6f4993c9389524a9724f15ad050602195d919d
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-07-27 16:12:15 +0300

    commit First Non-Repeating Character project

---
+++ First.NonRepeating.Character/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace First.NonRepeating.Character
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine();
+
+            char result = GetFirstNonRepeatingCharacter(text);
+
+            Console.WriteLine(result);
+        }
+
+        private static char GetFirstNonRepeatingCharacter(string text)
+        {
+            int length = text.Length;
+
+            Dictionary<char, int> occurencesByChar = new Dictionary<char, int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                char c = text[i];
+
+                if (occurencesByChar.ContainsKey(c))
+                {
+                    occurencesByChar[c]++;
+                }
+                else
+                {
+                    occurencesByChar.Add(c, 1);
+                }
+            }
+
+            foreach (var kvp in occurencesByChar)
+            {
+                if (kvp.Value == 1)
+                {
+                    return kvp.Key;
+                }
+            }
+
+            return '_';
+        }
+    }
+}
