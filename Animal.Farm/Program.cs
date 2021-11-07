eee30ad0418f63df1d96cbff3bab05d065babfb5
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-11-07 18:52:05 +0200

    commit Animal.Farm project

---
+++ Animal.Farm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using AnimalFarm.Models;
+
+namespace AnimalFarm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var name = Console.ReadLine();
+                var age = int.Parse(Console.ReadLine());
+
+                var chicken = new Chicken(name, age);
+
+                Console.WriteLine(
+                    $"Chicken {chicken.Name} (age {chicken.Age}) can produce {chicken.ProductPerDay} eggs per day.");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
