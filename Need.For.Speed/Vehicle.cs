300942c1e765dfd2898b2e3dd700553da086a3b0
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-11-07 18:38:41 +0200

    commit Need.For.Speed project

---
+++ Need.For.Speed/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private int horsePower;
+        private double fuel;
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public int HorsePower
+        {
+            get
+            {
+                return this.horsePower;
+            }
+            set
+            {
+                this.horsePower = value;
+            }
+        }
+
+        public double Fuel
+        {
+            get
+            {
+                return this.fuel;
+            }
+            set
+            {
+                this.fuel = value;
+            }
+        }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        
+        public virtual void Drive(int kilometers)
+        {
+            this.Fuel -= kilometers * this.FuelConsumption;
+        }
+    }
+}
