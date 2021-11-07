aa34d2f5636fc9fc012b09001439b0ab1e77f38e
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-11-07 18:36:34 +0200

    commit Person project

---
+++ Person/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            if (age < 0)
+            {
+                throw new Exception();
+            }
+
+            Person person;
+
+            if (age <= 15)
+            {
+                person = new Child(name, age);
+            }
+            else
+            {
+                person = new Person(name, age);
+            }
+
+            Console.WriteLine(person);
+        }
+    }
+}
