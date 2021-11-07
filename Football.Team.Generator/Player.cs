572a317450a301291aea4ab8c903d08a4316cea9
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-11-07 18:54:27 +0200

    commit Football.Team.Generator project

---
+++ Football.Team.Generator/Player.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Football.Team.Generator
+{
+    public class Player
+    {
+        private string name;
+        private int endurance;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+
+        public Player(string name, int endurance, int sprint, int dribble, int passing, int shooting)
+        {
+            this.Name = name;
+            this.Endurance = endurance;
+            this.Sprint = sprint;
+            this.Dribble = dribble;
+            this.Passing = passing;
+            this.Shooting = shooting;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("A name should not be empty.");
+                }
+
+                this.name = value;
+            }
+        }
+        public int Endurance
+        {
+            get => this.endurance;
+            private set
+            {
+                ValidateStat(nameof(Endurance), value);
+                this.endurance = value;
+            }
+        }
+
+        public int Sprint
+        {
+            get => this.sprint;
+            private set
+            {
+                ValidateStat(nameof(Sprint), value);
+                this.sprint = value;
+            }
+        }
+
+        public int Dribble
+        {
+            get => this.dribble;
+            private set
+            {
+                ValidateStat(nameof(Dribble), value);
+                this.dribble = value;
+            }
+        }
+
+        public int Passing
+        {
+            get => this.passing;
+            private set
+            {
+                ValidateStat(nameof(Passing), value);
+                this.passing = value;
+            }
+        }
+
+        public int Shooting
+        {
+            get => this.shooting;
+            private set
+            {
+                ValidateStat(nameof(Shooting), value);
+                this.shooting = value;
+            }
+        }
+
+        public double Stats =>
+            (Endurance + Sprint + Dribble + Passing + Shooting) / 5.0;
+
+        private void ValidateStat(string varName, int value)
+        {
+            if (value < 0 || value > 100)
+            {
+                throw new ArgumentException($"{varName} should be between 0 and 100.");
+            }
+        }
+    }
+}
---
+++ Football.Team.Generator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Football.Team.Generator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var teams = new Dictionary<string, Team>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "END")
+                {
+                    break;
+                }
+
+                var info = line.
+                    Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+                var action = info[0];
+
+                try
+                {
+                    if (action == "Team")
+                    {
+                        var name = info[1];
+                        var team = new Team(name);
+                        teams.Add(name, team);
+                    }
+                    else if (action == "Add")
+                    {
+                        var teamName = info[1];
+
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            Console.WriteLine($"Team {teamName} does not exist.");
+                            continue;
+                        }
+
+                        var playerName = info[2];
+                        int endurance = int.Parse(info[3]);
+                        int sprint = int.Parse(info[4]);
+                        int dribble = int.Parse(info[5]);
+                        int passing = int.Parse(info[6]);
+                        int shooting = int.Parse(info[7]);
+
+                        var player = new Player(playerName, endurance, sprint, dribble, passing, shooting);
+                        teams[teamName].AddPlayer(player);
+                    }
+                    else if (action == "Remove")
+                    {
+                        var teamName = info[1];
+                        var playerName = info[2];
+
+                        teams[teamName].RemovePlayer(playerName);
+                    }
+                    else if (action == "Rating")
+                    {
+                        var teamName = info[1];
+
+                        if (!teams.ContainsKey(teamName))
+                        {
+                            Console.WriteLine($"Team {teamName} does not exist.");
+                            continue;
+                        }
+
+                        Console.WriteLine($"{teamName} - {teams[teamName].Rating}");
+                    }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }               
+            }
+        }
+    }
+}
