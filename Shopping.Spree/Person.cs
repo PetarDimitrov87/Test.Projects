402329e697273c90e8876d27a60f0e2c6efb1acc
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2021-11-07 18:53:17 +0200

    commit Shopping.Spree project

---
+++ Shopping.Spree/Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shopping.Spree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+
+            this.products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => this.money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                this.money = value;
+            }
+        }
+
+        public IReadOnlyCollection<Product> Products =>
+            this.products;
+
+        public void AddProduct(Product product)
+        {
+            this.Money -= product.Cost;
+            this.products.Add(product);
+        }
+    }
+}
---
+++ Shopping.Spree/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shopping.Spree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            try
+            {
+                var people = new Dictionary<string, Person>();
+                var products = new Dictionary<string, Product>();
+
+                var peopleInfo = Console.ReadLine()
+                    .Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+                var productsInfo = Console.ReadLine()
+                    .Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var current in peopleInfo)
+                {
+                    var values = current.
+                        Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+                    var name = values[0];
+                    var money = decimal.Parse(values[1]);
+
+                    var person = new Person(name, money);
+                    people.Add(person.Name, person);
+                }
+
+                foreach (var current in productsInfo)
+                {
+                    var values = current.
+                        Split('=', StringSplitOptions.RemoveEmptyEntries);
+
+                    var name = values[0];
+                    var cost = decimal.Parse(values[1]);
+
+                    var product = new Product(name, cost);
+                    products.Add(product.Name, product);
+                }
+
+                while (true)
+                {
+                    var line = Console.ReadLine();
+
+                    if (line == "END")
+                    {
+                        break;
+                    }
+
+                    var purchaseInfo = line
+                        .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    var personName = purchaseInfo[0];
+                    var productName = purchaseInfo[1];
+
+                    var person = people[personName];
+                    var product = products[productName];
+
+                    if (person.Money - product.Cost < 0)
+                    {
+                        Console.WriteLine($"{person.Name} can't afford {product.Name}");
+                        continue;
+                    }
+
+                    person.AddProduct(product);
+                    Console.WriteLine($"{personName} bought {productName}");
+                }
+
+                foreach (var person in people)
+                {
+                    var productMessage = person.Value.Products.Count == 0
+                        ? "Nothing bought"
+                        : string.Join(", ", person.Value.Products.Select(x => x.Name));
+
+                    Console.WriteLine($"{person.Key} - {productMessage}");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }           
+        }
+    }
+}
