b61fb191ea87c54423caa7690c7cd891e949b97f
Petar Dimitrov <Petar Dimitrov@ASUS-E403NA>
2022-01-15 23:05:00 +0200

    commit test changes

--- DevOcean.AcademyTask/Program.cs
+++ DevOcean.AcademyTask/Program.cs
@@ -5,9 +5,6 @@ public static class Program
     {
         // See https://aka.ms/new-console-template for more information
         Console.WriteLine("Hello, World!");
-
-        var a = 5;
-        var b = 10;
     }
 
 }
